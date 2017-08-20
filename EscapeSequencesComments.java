e6fe1c5ccb73809b653cc261dc96a6350d4fcbb0
tman80 <tcampbell100@hotmail.com>
2017-08-20 19:47:37 +0100

    adding file structure

---
+++ EscapeSequencesComments.java
@@ -0,0 +1,21 @@
+public class EscapeSequencesComments {
+	public static void main( String[] args ) {
+		// This excercise demonstrates escape sequences & comments (like these)!
+		System.out.println( "Learn\tJava\n\tthe\nHand\tWay\n\n" );
+		System.out.println( "\tLearn Java the \"Hard\" Way!\n" );
+		// System.out.frimp( "Learn Java the Hard Way" );
+		System.out.println( "Hello\n" ); // This line prints Hello.
+		System.out.println( "Jello\by\n" ); // This line prints Jelly.
+	    /* The quick brown fox jumped over a lazy dog.
+	       Quick wafting zephyrs vex bold Jim. */ 
+		System.out /* testing */ .println( "Hard to believe, eh?" );
+		System.out.println( "Surprised? /* abcde */ Or what did you expect?" );
+		System.out.println( "\\ // -=- \\ //" );
+		System.out.println( "\\\\ \\\\\\ \\\\\\\\" ); //it takes 2 to make 1
+		System.out.print( "I hope understand \" escape sequences\" now.\n" );
+		//and comments. :)	
+	
+	}
+	//tried to add a block comment in the middle of println on line 13, it didn't work.
+	
+}
